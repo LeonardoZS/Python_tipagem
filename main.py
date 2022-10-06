dbaa192fa5e52e1866c4c5e45b9a9511d3f1a847
LeonardoZS <leonardozaniboni@usp.br>
2022-10-06 12:27:43 -0300

    Segundo commit : Teste realizado com oop e tipagem

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-a = input()
-print(type(int(a)))
+#no EMACS
+# C-c C-c -> roda o programa
+# C-c C-p -> inicia o interpretador python
+# C-c C-r -> roda o que esta selecionado
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def latir(self):
+        print('auau')
 
 
+cachorro = Dog('jorjao',10)
+
+print(cachorro.age)
+cachorro.latir()
+
+print(f"teste é esse : {type(cachorro.name)}")
 
 
